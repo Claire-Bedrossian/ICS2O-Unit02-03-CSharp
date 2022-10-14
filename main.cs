3e1ab7f3b36fba4b284e690d7022a4f81f8bbb0d
Claire-Bedrossi <113055648+Claire-Bedrossian@users.noreply.github.com>
2022-10-14 11:30:50 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,34 @@
+// Created by: Claire Bedrossian
+// Created on: Oct 2022
+//
+// This program calculates the perimeter and area of a rectangle
+
+// Created by: Mr Coxall
+// Created on: Sep 2020
+//
+// This program accepts user input
+
+using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        // This function accepts user input
+        string streetName;
+        int streetNumber;
+
+        Console.WriteLine("This program gets a user's address.");
+        Console.WriteLine("");
+
+        Console.Write("Enter your street name: ");
+        streetName = Console.ReadLine();
+        Console.Write("Enter your house number: ");
+        streetNumber = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("");
+        Console.WriteLine("Your info is: " + streetNumber +" "+ streetName + ".");
+
+        Console.WriteLine("\nDone.");
+    }
+}
